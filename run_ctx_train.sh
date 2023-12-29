a6c7feb43b47f1503423f4835ca7a110ba74a40e
Vaishnavi Bhargava <47900863+vaishnavi026@users.noreply.github.com>
2023-12-29 09:27:35 -0600

    Add files via upload

---
+++ run_ctx_train.sh
@@ -0,0 +1,6 @@
+# CUDA_VISIBLE_DEVICES=0 python3 main.py      --caption_file data/instruct_captions.json     --model declare-lab/flan-alpaca-base     --user_msg rationale --img_type vit     --bs 32 --eval_bs 32 --epoch 20 --lr 8e-5 --output_len 512     --use_generate --final_eval --prompt_format QCM-E     --output_dir experiments     --evaluate_dir models/mm-cot-base-rationale
+# CUDA_VISIBLE_DEVICES=0 python main.py      --caption_file data/instruct_captions.json     --model declare-lab/flan-alpaca-base     --user_msg rationale --img_type vit     --bs 8 --eval_bs 8 --epoch 20 --lr 8e-5 --output_len 64     --use_generate --prompt_format QCMG-A     --output_dir experiments     --eval_le models/mm-cot-base-rationale/predictions_ans_eval.json     --test_le models/mm-cot-base-rationale/predictions_ans_test.json     --evaluate_dir models/mm-cot-base-ans
+
+
+
+CUDA_VISIBLE_DEVICES=0 python main.py --caption_file data/instruct_captions.json --model declare-lab/flan-alpaca-base --user_msg rationale --img_type vit --bs 16 --eval_bs 16 --epoch 20 --lr 8e-5 --input_len 448 --n_ctx 64 --output_len 64 --use_generate --prompt_format QCMG-A --output_dir experiments --eval_le models/mm-cot-base-rationale/predictions_ans_eval.json --test_le models/mm-cot-base-rationale/predictions_ans_test.json
